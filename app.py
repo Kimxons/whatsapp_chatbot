5292026c3e7c317b71994f240c449cb01a9bc531
kimxons <kitongameshack9@gmail.com>
2020-05-26 14:15:05 -0400

    modified app.py

--- app.py
+++ app.py
@@ -21,18 +21,8 @@ def index():
         msg.body(quote)
         responded = True
 
-    # if 'summary' in new_msg:
-    #     r = requests.get('https://api.covid19api.com/summary')
-    #     if r.status_code == 200:
-    #         data = r.json()
-    #         summary = (f'{data["content"]}')
-    #     else:
-    #         summary = "There is no information available about corona virus"
-    #     msg.body(summary)        
-    #     responded = True
-
     if not responded:
-        msg.body('I am sorry i am not knowledgeble in this!')
+        msg.body('I am sorry i can only help with quotes!')
     return str(response)
 
 if __name__ == '__main__':
