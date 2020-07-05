5f52783904589dcd110f149ba85a7812bb50928d
kimxons <kitongameshack9@gmail.com>
2020-07-05 14:54:39 +0300

    requirements.txt

---
+++ logic.py
@@ -0,0 +1,8 @@
+responnded = False
+if "quote" in new_msg:
+    #quote response
+    responded = True
+else:
+    #add error message to be displayed
+if not responded:
+    #a text to be returned as response
